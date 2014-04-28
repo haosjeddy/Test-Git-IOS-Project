d78ac20fa7b8017b3ebcf42e3265ca47d4d81ed4
Jeddy Hao <haosjeddy@gmail.com>
2014-04-28 17:46:26 -0400

    first commit

---
+++ Git Test/JHViewController.h
@@ -0,0 +1,13 @@
+//
+//  JHViewController.h
+//  Git Test
+//
+//  Created by Jeddy Hao on 4/28/14.
+//  Copyright (c) 2014 Tiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JHViewController : UIViewController
+
+@end
---
+++ Git Test/JHViewController.m
@@ -0,0 +1,29 @@
+//
+//  JHViewController.m
+//  Git Test
+//
+//  Created by Jeddy Hao on 4/28/14.
+//  Copyright (c) 2014 Tiger. All rights reserved.
+//
+
+#import "JHViewController.h"
+
+@interface JHViewController ()
+
+@end
+
+@implementation JHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
